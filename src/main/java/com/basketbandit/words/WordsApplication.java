d3b7d744d8ceba09c47d6be55f91b6c5c9c10e97
BasketBandit <errorsfound@hotmail.co.jp>
2022-02-22 16:05:22 +0000

    parse words from file / sort into separate lists

--- src/main/java/com/basketbandit/words/WordsApplication.java
+++ src/main/java/com/basketbandit/words/WordsApplication.java
@@ -1,11 +1,31 @@
 package com.basketbandit.words;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @SpringBootApplication
 public class WordsApplication {
+	private static final Logger log = LoggerFactory.getLogger(WordsApplication.class);
+	public static final HashMap<Integer, ArrayList<String>> words = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(WordsApplication.class, args);
+	}
+
+	public WordsApplication() throws IOException {
+		log.info("Parsing words from /static/data/2019scrabble3plus.txt");
+		new BufferedReader(new FileReader(new ClassPathResource("static/data/2019scrabble3plus.txt").getFile())).lines().forEach(word -> {
+			words.computeIfAbsent(word.length(), k -> new ArrayList<>()).add(word);
+		});
+		log.info("Found words of length " + words.keySet());
 	}
 }

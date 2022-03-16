71cbc2440866ca2a1e0ae8f96ec56944d46204a2
BasketBandit <errorsfound@hotmail.co.jp>
2022-03-16 16:17:36 +0000

    generify project for heardle integration / changed paths in rest controller

--- src/main/java/com/basketbandit/words/rest/RestfulController.java
+++ src/main/java/com/basketbandit/words/rest/RestfulController.java
@@ -19,23 +19,28 @@ public class RestfulController {
 
     @GetMapping("/")
     public ModelAndView root() {
-        ModelAndView modelAndView = new ModelAndView("index");
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/game/words/")
+    public ModelAndView words() {
+        ModelAndView modelAndView = new ModelAndView("/words/index");
         modelAndView.addObject("letters", words.keySet());
         return modelAndView;
     }
 
-    @GetMapping("/game/{x}")
+    @GetMapping("/game/words/{x}")
     public ModelAndView play(@PathVariable String x) {
         int type = Sanitiser.isNumeric(x) ? Integer.parseInt(x) : 5;
         ArrayList<String> list = words.getOrDefault(type, words.get(5)); // this should ensure that all possible inputs will resolve
-        ModelAndView modelAndView = new ModelAndView("play");
+        ModelAndView modelAndView = new ModelAndView("/words/play");
         modelAndView.addObject("words", list);
         modelAndView.addObject("word", list.get(new Random(System.currentTimeMillis()).nextInt(list.size()-1)));
         modelAndView.addObject("length", type);
         return modelAndView;
     }
 
-    @GetMapping("/words/{x}")
+    @GetMapping("/game/words/list/{x}")
     public ArrayList<String> words(@PathVariable String x) {
         int type = Sanitiser.isNumeric(x) ? Integer.parseInt(x) : 5;
         return words.getOrDefault(type, words.get(5));

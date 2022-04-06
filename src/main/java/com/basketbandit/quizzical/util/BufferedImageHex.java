b60d71cf616f181dae90afd5f63ecbe078798be6
BasketBandit <errorsfound@hotmail.co.jp>
2022-04-06 13:37:50 +0100

    convert image into html canvas bringing much speed and lower data usage

--- src/main/java/com/basketbandit/quizzical/util/BufferedImageHex.java
+++ src/main/java/com/basketbandit/quizzical/util/BufferedImageHex.java
@@ -1,18 +1,27 @@
 package com.basketbandit.quizzical.util;
 
+import com.basketbandit.quizzical.Application;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.util.Base64;
 
 public class BufferedImageHex extends BufferedImage {
     public BufferedImageHex(int width, int height, int imageType) {
         super(width, height, imageType);
     }
 
-    public String getHex(int x, int y) {
-        StringBuilder hex = new StringBuilder(Integer.toHexString(getRGB(x, y) & 0xffffff));
-        while(hex.length() < 6) {
-            hex.insert(0, "0");
+    public String getAsBase64Png() {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(OutputStream outputStream = Base64.getEncoder().wrap(byteArrayOutputStream)) {
+            ImageIO.write(Application.image, "png", outputStream);
+        } catch (final IOException ioe) {
+            throw new UncheckedIOException(ioe);
         }
-        hex.insert(0, "#");
-        return hex.toString();
+        return byteArrayOutputStream.toString();
     }
 }

dce777e55882790ea4008062d3b104923928f0e5
BasketBandit <errorsfound@hotmail.co.jp>
2022-04-21 01:17:50 +0100

    update SoundsController to pick a random sound

--- src/main/java/com/basketbandit/apples/rest/SoundsController.java
+++ src/main/java/com/basketbandit/apples/rest/SoundsController.java
@@ -7,17 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 @RestController
 public class SoundsController {
     private static final Logger log = LoggerFactory.getLogger(WordsController.class);
-    private static final HashMap<Integer, ArrayList<String>> words = Application.words;
+    private static final HashMap<String, String> sounds = Application.sounds;
 
     @GetMapping("/sounds/")
     public ModelAndView words() {
         ModelAndView modelAndView = new ModelAndView("./sounds/index");
+        modelAndView.addObject("sounds", sounds.values());
+        modelAndView.addObject("sound", sounds.keySet().toArray()[new Random(System.currentTimeMillis()).nextInt(sounds.size()-1)]);
         return modelAndView;
     }
 }

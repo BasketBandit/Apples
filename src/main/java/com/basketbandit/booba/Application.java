1dcc254fd52e3b3ecde2d7f9fee5f4b9002ec525
BasketBandit <errorsfound@hotmail.co.jp>
2023-05-20 23:15:40 +0100

    disable discord bot

--- src/main/java/com/basketbandit/booba/Application.java
+++ src/main/java/com/basketbandit/booba/Application.java
@@ -1,6 +1,5 @@
 package com.basketbandit.booba;
 
-import com.basketbandit.discord.Gumbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		Gumbo gumbo = new Gumbo();
 	}
 }

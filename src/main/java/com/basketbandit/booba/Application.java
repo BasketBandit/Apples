7134f4a732654facf848415bd39b128211774b3f
BasketBandit <errorsfound@hotmail.co.jp>
2023-05-20 22:32:39 +0100

    move gumbo to main

--- src/main/java/com/basketbandit/booba/Application.java
+++ src/main/java/com/basketbandit/booba/Application.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	Gumbo gumbo = new Gumbo();
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		Gumbo gumbo = new Gumbo();
 	}
 }

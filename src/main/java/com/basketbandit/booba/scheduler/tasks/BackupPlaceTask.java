afda845b12e0ab83ddf3492b798d9a63079f7971
BasketBandit <errorsfound@hotmail.co.jp>
2023-07-20 19:09:38 +0100

    disable discord backups for now

--- src/main/java/com/basketbandit/booba/scheduler/tasks/BackupPlaceTask.java
+++ src/main/java/com/basketbandit/booba/scheduler/tasks/BackupPlaceTask.java
@@ -1,13 +1,12 @@
 package com.basketbandit.booba.scheduler.tasks;
 
-import com.basketbandit.booba.rest.PlaceController;
 import com.basketbandit.booba.scheduler.Task;
 
 public class BackupPlaceTask implements Task {
     @Override
     public void run() {
         try {
-            PlaceController.backupToDiscord();
+            //PlaceController.backupToDiscord();
         } catch(Exception e) {
             log.error("Unable to backup, reason: {}", e.getMessage(), e);
         }

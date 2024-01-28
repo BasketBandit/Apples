cc6486ce3f1cfa9b3161c83b57fd0dd692fbc6b7
BasketBandit <errorsfound@hotmail.co.jp>
2024-01-28 22:49:05 +0000

    remove gumbo

--- src/main/java/com/basketbandit/booba/rest/PlaceController.java
+++ src/main/java/com/basketbandit/booba/rest/PlaceController.java
@@ -1,9 +1,7 @@
 package com.basketbandit.booba.rest;
 
 import com.basketbandit.booba.scheduler.ScheduleHandler;
-import com.basketbandit.booba.scheduler.jobs.BackupJob;
 import com.basketbandit.booba.scheduler.jobs.UpdateJob;
-import com.basketbandit.booba.scheduler.tasks.BackupPlaceTask;
 import com.basketbandit.booba.scheduler.tasks.UpdateImageTask;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.InputStream;
-import java.sql.Time;
-import java.time.Instant;
-import java.util.Arrays;
 
 @RestController
 public class PlaceController implements Controller {
@@ -29,7 +21,6 @@ public class PlaceController implements Controller {
             log.info("Loading image form ./data/canvas.png");
             image.setData(ImageIO.read(new File("./data/canvas.png")).getRaster()); // BufferedImage cannot be cast; we have to set data manually
             ScheduleHandler.registerJob(new UpdateJob(new UpdateImageTask())); // creates a job to save changes of the canvas to disk
-            ScheduleHandler.registerJob(new BackupJob(new BackupPlaceTask()));
             log.info("Successfully loaded image.");
         } catch(Exception e) {
             log.warn("Failed to load existing image, reason: {}", e.getMessage());
@@ -45,21 +36,6 @@ public class PlaceController implements Controller {
             ImageIO.write(image, "png", new File("./data/canvas.png"));
         } catch(Exception e) {
             log.error("Something went wrong while saving canvas.png... {}", e.getMessage(), e);
-        }
-    }
-
-    public static void backupToDiscord() {
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", os);
-            byte[] newByteArray = os.toByteArray();
-            if(!Arrays.equals(imageByteArray, newByteArray)) {
-                imageByteArray = newByteArray;
-                InputStream inputStream = new ByteArrayInputStream(imageByteArray);
-                GumboController.sendFile(1114649660003450910L, inputStream, Time.from(Instant.now()).toString()+".png");
-            }
-        } catch(Exception e) {
-            log.error("Something went wrong! {}", e.getMessage(), e);
         }
     }
 
